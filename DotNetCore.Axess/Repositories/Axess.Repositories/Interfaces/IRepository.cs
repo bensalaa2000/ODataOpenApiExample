63c206122c5658292883c9f515e8f94f00e17d8b
Abdelhakim Bensalah <bensalaa2000@gmail.com>
2023-06-13 23:54:22 +0200

    structure

--- DotNetCore.Axess/Repositories/Axess.Repositories/Interfaces/IRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using DotNetCore.Axess.Domain;
-
-namespace DotNetCore.Axess.Repositories.Interfaces;
-
-/// <summary>
-/// Représente l'ensemble des méthodes pour gérer une entité.
-/// </summary>
-/// <typeparam name="TEntity">L'entité à gérer.</typeparam>
-public interface IRepository<TEntity, TId> : ICommandRepository<TEntity, TId>, IQueryRepository<TEntity, TId> where TEntity : Entity<TId> { }
